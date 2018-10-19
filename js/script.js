46178a25bc942496fbe6bba402a81bb7521a6505
Greg <greggordon98@gmail.com>
2018-10-19 02:04:26 -0700

    stuck on credit card
    stuck on credit card
    
    i can delete all code for credit card if it is not working.

--- js/script.js
+++ js/script.js
@@ -139,3 +139,90 @@ $('.activities label').change('click', function(){
 //If they add 1 workshop, the total should change to Total: $300.
 
 // ***************   Register for Activities SECTION END *************************
+
+
+// ***************   PAYMENT SECTION START *************************
+
+/*"Payment Info" section
+Display payment sections based on the payment option chosen in the select menu.
+*/
+
+
+/*
+The "Credit Card" payment option should be selected by default. Display the #credit-card div,
+and hide the "PayPal" and "Bitcoin" information. Payment option in the select menu should match
+ the payment option displayed on the page.
+*/
+const creditCard = $('#payment option:nth-child(2)');
+//$('#payment option').eq(1);
+const payPal = $('#payment option:nth-child(3)');
+const bitCoin = $('#payment option:nth-child(4)');
+const paymentTypes = $('payment');
+const cc = $('#credit-card');
+console.log(creditCard);
+$(creditCard).is('click', function(){
+  if($(creditCard) === true){
+    $(cc).hide();
+  } else if(($(creditCard) === false)){
+    $(cc).show();
+  }
+});
+
+
+
+//
+// if ($(creditCard).is(':checked') === true) {
+//   $('#credit-card').show();
+// }else if ($(creditCard).is(':checked') === false) {
+//   $('#credit-card').hide();
+//  }
+//
+// ////////////////////////////////////////////////////////////////////////////////////
+//
+// if ($(payPal).is(':checked') === true) {
+//
+// } else if ($(payPal).is(':checked') === false) {
+//
+// }
+//
+// ////////////////////////////////////////////////////////////////////////////////////
+//
+// if ($(bitCoin).is(':checked') === true) {
+//
+// } else if ($(bitCoin).is(':checked') === false) {
+//
+// }
+//
+//
+// /*
+// When a user selects the "PayPal" payment option, the PayPal information should display,
+//  and the credit card and “Bitcoin” information should be hidden.
+// */
+//
+//
+//
+// /*When a user selects the "Bitcoin" payment option, the Bitcoin information should display,
+// and the credit card and “PayPal” information should be hidden.
+// */
+//
+//
+//
+// /*
+// note: The user should not be able to select the "Select Payment Method" option from
+//  the payment select menu, because the user should not be able to submit the form without
+//   a chosen payment option.
+// */
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // ***************   PAYMENT SECTION END *************************
